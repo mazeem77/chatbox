548fd4632a053eac13a681e796030d1644b682cd
mazeem77 <mazeem8856@gmail.com>
2021-12-26 14:08:56 +0500

    Added FrontEnd/BackEnd Folder

---
+++ frontEnd/src/Components/Dock/DashBoard/DashBoard.js
@@ -0,0 +1,12 @@
+import '../DashBoard/DashBoard.css'
+
+function DashBoard(){
+    return(
+        <div className='bigBox'>
+            <div className="column">
+            </div>
+        </div>
+    );
+}
+
+export default DashBoard;
---
+++ frontEnd/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Dock from './Components/Dock/Dock';
+import LoginPage from './Components/LoginPage/LoginPage';
+
+ReactDOM.render(
+    <Dock />,
+  document.getElementById('root')
+);
